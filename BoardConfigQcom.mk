c7800fd2bca65d230c17cc926c8cc7ac3ce861f0
ProjectFlare-OSS <projectflare2000@gmail.com>
2026-03-15 17:30:23 +0530

    qcom: Move platform defs to a new file
    * also add target kernel version for common qti components to identify correct dir.
    
    Change-Id: I08a5373d1cd3630024d19597f3e703bb41ebeaa8

--- BoardConfigQcom.mk
+++ BoardConfigQcom.mk
@@ -1,48 +1,4 @@
-include hardware/qcom-caf/common/qcom_boards.mk
-include hardware/qcom-caf/common/qcom_defs.mk
-
-UM_3_18_HAL_FAMILY := msm8996
-UM_4_4_HAL_FAMILY := msm8998
-
-ifeq (,$(TARGET_ENFORCES_QSSI))
-UM_3_18_HAL_FAMILY += msm8937 msm8953
-UM_4_4_HAL_FAMILY += sdm660
-else
-UM_4_9_LEGACY_FAMILY := msm8937 msm8953
-UM_4_19_LEGACY_FAMILY := sdm660
-endif
-
-UM_PLATFORMS := \
-    $(UM_3_18_FAMILY) \
-    $(UM_4_4_FAMILY) \
-    $(UM_4_9_FAMILY) \
-    $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
-    $(UM_5_4_FAMILY) \
-    $(UM_5_10_FAMILY) \
-    $(UM_5_15_FAMILY) \
-    $(UM_6_1_FAMILY) \
-    $(UM_6_6_FAMILY)
-
-LEGACY_UM_PLATFORMS := \
-    $(UM_3_18_FAMILY) \
-    $(UM_4_4_FAMILY) \
-    $(UM_4_9_FAMILY) \
-    $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
-    $(UM_5_4_FAMILY)
-
-QSSI_SUPPORTED_PLATFORMS := \
-    $(UM_4_9_LEGACY_FAMILY) \
-    $(UM_4_19_LEGACY_FAMILY) \
-    $(UM_4_9_FAMILY) \
-    $(UM_4_14_FAMILY) \
-    $(UM_4_19_FAMILY) \
-    $(UM_5_4_FAMILY) \
-    $(UM_5_10_FAMILY) \
-    $(UM_5_15_FAMILY) \
-    $(UM_6_1_FAMILY) \
-    $(UM_6_6_FAMILY)
+include hardware/qcom-caf/common/qcom_platform_defs.mk
 
 BOARD_USES_ADRENO := true
 
@@ -347,34 +303,6 @@ endif
 $(call soong_config_set,qtidisplay,gralloc_handle_has_custom_content_md_reserved_size,$(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE))
 $(call soong_config_set,qtidisplay,gralloc_handle_has_reserved_size,$(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE))
 $(call soong_config_set,qtidisplay,gralloc_handle_has_ubwcp_format,$(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT))
-
-ifneq ($(filter $(UM_3_18_HAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := msm8996
-else ifneq ($(filter $(UM_4_9_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := msm8953
-else ifneq ($(filter $(UM_4_4_HAL_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := msm8998
-else ifneq ($(filter $(UM_4_19_LEGACY_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sdm660
-else ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sdm845
-else ifneq ($(filter $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8150
-else ifneq ($(filter $(UM_4_19_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8250
-else ifneq ($(filter $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8350
-else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8450
-else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8550
-else ifneq ($(filter $(UM_6_1_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8650
-else ifneq ($(filter $(UM_6_6_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8750
-else
-    QCOM_HARDWARE_VARIANT := $(TARGET_BOARD_PLATFORM)
-endif
 
 # Pass board platform to kernel build
 TARGET_KERNEL_ADDITIONAL_FLAGS += TARGET_BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
